623f2ec638fd8033ab9ffa93d3f8d66e63531804
talhakavaz <145286277+talhakavaz@users.noreply.github.com>
2023-10-12 13:37:40 -0400

    Update Item.java

--- src/main/java/com/mycompany/fantasygame/Item.java
+++ src/main/java/com/mycompany/fantasygame/Item.java
@@ -6,10 +6,43 @@ package com.mycompany.fantasygame;
 
 /**
  *
- * @author Arid and Talha
+ * @author Arid
  */
 public class Item {
     private String itemname;
     private double cost;
     private int level;
+
+    public Item(String itemname, double cost, int level) {
+        this.itemname = itemname;
+        this.cost = cost;
+        this.level = level;
+    }
+
+    public String getItemname() {
+        return itemname;
+    }
+
+    public void setItemname(String itemname) {
+        this.itemname = itemname;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    
+
 }
+
